0f3b4f483c9f1f4863fb9e5c150ea7c6ea059aad
michaelvong <mvong006@ucr.edu>
2024-06-11 20:10:12 -0700

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,14 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/layout"
+import { useSession} from "next-auth/react"
 
 export default function Home() {
-  const { data: session } = useSession()
-  if(!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
-        </div>
-      </div>
-    );
-  }
+  const {data: session} = useSession();
+  console.log({session})
   return (
-    <div>logged in {session.user.email}</div>
-  );
+    <Layout>
+      <div className="text-blue-900 flex">
+        Hello, {session?.user?.name}
+      </div>
+    </Layout>
+  )
 }
