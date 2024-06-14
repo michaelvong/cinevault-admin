afe7724bb4104519773be7884d4c09acece60426
michaelvong <mvong006@ucr.edu>
2024-06-13 19:47:59 -0700

    changes

--- pages/products/edit/[...id].js
+++ pages/products/edit/[...id].js
@@ -1,21 +1,25 @@
 import Layout from "@/components/layout";
 import {useRouter} from "next/router";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import ProductForm from "@/components/ProductForm";
 export default function EditProductPage(){
+    const [productInfo, setProductInfo] = useState(null);
     const router = useRouter();
     const {id} = router.query;
     useEffect(() => {
         if(!id){
             return;
         }
-        axios.get('api/products?id=' + id).then(response => {
-            console.log(response.data);
+        axios.get('/api/products?id=' + id).then(response => {
+            setProductInfo(response.data);
         });
     }, [id]);
     return(
         <Layout>
-            edit
+            <h1>Edit Product</h1>
+            {productInfo && (<ProductForm {...productInfo}/>)}
+            
         </Layout>
     );
 }
