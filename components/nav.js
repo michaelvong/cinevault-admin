0076460b7bda113ad7c9aff7d6d7e01d3fcdceac
michaelvong <mvong006@ucr.edu>
2024-06-15 23:03:32 -0700

    added logout btn

--- components/nav.js
+++ components/nav.js
@@ -1,10 +1,16 @@
-import Link from "next/link"
-import {useRouter} from "next/router"
+import Link from "next/link";
+import {useRouter} from "next/router";
+import {signOut} from "next-auth/react";
 export default function Nav(){
     const inactiveLink = 'flex gap-1 p-1';
     const activeLink = inactiveLink + ' bg-white text-blue-900 rounded-l-lg';
     const router = useRouter();
     const {pathname} = router;
+
+    async function logOut(){
+        await router.push('/');
+        await signOut();
+    }
     return (
         <aside className="text-white p-4 pr-0">
             <Link href={'/'} className="flex gap-1 mb-4 mr-4">
@@ -41,6 +47,12 @@ export default function Nav(){
                 </svg>
                 Settings
                 </Link>
+                <button  onClick = {() => logOut} className={inactiveLink}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+                </svg>
+                Logout
+                </button>
             </nav>
         </aside>
     );
